1e23f458123a0f06bcd31c6762303c440ddd11b1
Moonrez <icewaterfloe@gmail.com>
2023-08-20 17:26:57 -0700

    Ver. 1.1 bugfixes
    Fixed:
    - Some grown flowers didn't drop themselves
    - Some vanilla flowers were missing custom flower tags
    - Food saturation values were very wrong

--- src/main/java/com/moonrez/hybrid_flowers/item/ModFoods.java
+++ src/main/java/com/moonrez/hybrid_flowers/item/ModFoods.java
@@ -4,8 +4,9 @@ import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods
 {
+	// I have no idea how the saturationMod values correlate to actual saturation. I think it's some sort of multiplier.
 	public static final FoodProperties POPPYSEED_BAGEL =
-			(new FoodProperties.Builder().nutrition(5).saturationMod(7.0f).build());
+			(new FoodProperties.Builder().nutrition(5).saturationMod(0.65f).build());
 	public static final FoodProperties POPPYSEED_MUFFIN =
-			(new FoodProperties.Builder().nutrition(6).saturationMod(10.8f).build());
+			(new FoodProperties.Builder().nutrition(6).saturationMod(0.9f).build());
 }
